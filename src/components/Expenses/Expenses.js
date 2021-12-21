24da246858e04d6c78dfabaaf15701d4089088ff
Nishan Shetty <29223291+Brown-Boi-Nash@users.noreply.github.com>
2021-12-21 23:41:35 +0530

    Added Filter and enabled user input data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,38 +4,32 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const [filteredYear, setFilteredYear] = useState("2020")
+  const addYearFilter = (year) => {
+    setFilteredYear(year);
+  };
 
-  const addYearFilter = (year) =>{
-    setFilteredYear(year)
-  }
+  const filteredExpenses = props.data.filter((expense)=>{
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
 
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeYearFilter={addYearFilter}/>
-      <ExpenseItem
-        title={props.data[0].title}
-        amount={props.data[0].amount}
-        date={props.data[0].date}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeYearFilter={addYearFilter}
       />
-      <ExpenseItem
-        title={props.data[1].title}
-        amount={props.data[1].amount}
-        date={props.data[1].date}
-      />
-      <ExpenseItem
-        title={props.data[2].title}
-        amount={props.data[2].amount}
-        date={props.data[2].date}
-      />
-      <ExpenseItem
-        title={props.data[3].title}
-        amount={props.data[3].amount}
-        date={props.data[3].date}
-      />
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
-}
+};
 
 export default Expenses;
