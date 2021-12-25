4ec28382a2a602661615fee0c759ea5a9c205715
Nishan Shetty <29223291+Brown-Boi-Nash@users.noreply.github.com>
2021-12-25 18:03:20 +0530

    Adding condition if no expenses are present in a given year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,20 +14,26 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   })
 
+  let expensesContent = <p>No expenses found.</p>
+
+  if(filteredExpenses.length > 0){
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeYearFilter={addYearFilter}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 };
